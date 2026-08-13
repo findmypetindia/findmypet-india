f8e435f425a9d0f915bbd9a95c9e2fde7722d6ad
findmypetindia <indiafindmypet@gmail.com>
2026-08-13 10:43:05 +0530

    Remove remaining Google Analytics tracking from PWA

--- js/pwa.js
+++ js/pwa.js
@@ -1,23 +1,4 @@
 
-// Google Analytics (free GA4): visitor and page-view tracking.
-(function loadFindMyPetAnalytics() {
-  if (window.__findMyPetAnalyticsLoaded) return;
-  window.__findMyPetAnalyticsLoaded = true;
-
-  const measurementId = "G-1KW8WQV6WB";
-  const analyticsScript = document.createElement("script");
-  analyticsScript.async = true;
-  analyticsScript.src = "https://www.googletagmanager.com/gtag/js?id=" + measurementId;
-  document.head.appendChild(analyticsScript);
-
-  window.dataLayer = window.dataLayer || [];
-  window.gtag = window.gtag || function () {
-    window.dataLayer.push(arguments);
-  };
-  window.gtag("js", new Date());
-  window.gtag("config", measurementId);
-})();
-
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
